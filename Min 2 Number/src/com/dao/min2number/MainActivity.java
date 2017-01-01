fbf0b1b6e78fb73515e662d2175ffa92fb15cd1a
DaoNguyen <dao.nbm1502@gmail.com>
2017-01-01 21:43:29 +0700

    Lesson2_If_Switch
    Acquainted with statements if/switch in Java language

---
+++ Min 2 Number/src/com/dao/min2number/MainActivity.java
@@ -0,0 +1,55 @@
+package com.dao.min2number;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	private EditText edtNumberA, edtNumberB;
+	private Button btnCompare;
+	private TextView txtResult;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		//Mapped
+		onMapped();
+		
+		//Catch even when click button
+		onClickButton();
+	}
+
+	private void onClickButton() {
+		btnCompare.setOnClickListener(new View.OnClickListener() {	
+			@Override
+			public void onClick(View v) {
+				//Convert EditText to String and continue to Float number
+				Float numberA = Float.valueOf(edtNumberA.getText().toString());
+				Float numberB = Float.valueOf(edtNumberB.getText().toString());
+				Float min = 0f;
+				
+				//Compare to find min between 2 number A, B
+				if(numberA<numberB){
+					min=numberA;
+					txtResult.setText(String.valueOf(min));
+				}else{//numberA>=numberB
+					min=numberB;
+					txtResult.setText(String.valueOf(min));
+				}
+			}
+		});		
+	}
+
+	private void onMapped() {
+		edtNumberA = (EditText) findViewById(R.id.edtNumberA);	
+		edtNumberB = (EditText) findViewById(R.id.edtNumberB);	
+		btnCompare = (Button) findViewById(R.id.btnCompare);
+		txtResult = (TextView) findViewById(R.id.txtResult);
+	}
+}
